09f04282de4b6f67418fc73ab2ede8851c9c52fc
FULLMOON <boreum@iboleum-ui-MacBookAir.local>
2023-05-20 21:55:00 +0900

    model

--- ddj/ddjapp/models.py
+++ ddj/ddjapp/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+
+class Room(models.Model):
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room')
+    title = models.CharField(max_length=20)
+    current_member = models.IntegerField()
+    want_member = models.TextField()
+    finished = models.BooleanField()
+    # template = models.ForeignKey(Template, related_name = 'room')
+
+    def __str__(self):
+        return self.title
+    
+class Template(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='template')
+    talk_topic = models.CharField(max_length=20)
+    hobby = models.CharField(max_length=20)
+    age = models.IntegerField()
+    give_food = models.BooleanField()
+
+    def __str__(self):
+        return self.room.title
