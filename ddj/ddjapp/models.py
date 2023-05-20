4eb0c5aae95ceacf2d10107014c38b6befcb61e7
FULLMOON <boreum@iboleum-ui-MacBookAir.local>
2023-05-21 04:12:28 +0900

    models 수정

--- ddj/ddjapp/models.py
+++ ddj/ddjapp/models.py
@@ -4,23 +4,20 @@ from django.db import models
 class Room(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room')
     current_member = models.TextField()
-    want_member = models.TextField()
-    finished = models.BooleanField()
+    want_member = models.IntegerField()
+    matched = models.BooleanField(null=False)
     talk_topic = models.CharField(max_length=20)
     age = models.TextField()
     give_food = models.TextField()
     sex = models.CharField(max_length=50)
-    
-    request_member = models.IntegerField(null=True)
-    matched = models.BooleanField(null=False)
-
+   
     def __str__(self):
         return self.give_food
 
 class Request(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room2')
-    reciever = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='Request')
-    req_member = models.IntegerField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent')
+    reciever = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='recieved')
+    req_member = models.TextField()
 
     def __str__(self):
         return self.sender
