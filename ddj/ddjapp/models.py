8442b7470a4846766c1538d71adec207a37bbf59
FULLMOON <boreum@iboleum-ui-MacBookAir.local>
2023-05-20 22:20:25 +0900

    model 수정

--- ddj/ddjapp/models.py
+++ ddj/ddjapp/models.py
@@ -8,7 +8,7 @@ class Room(models.Model):
     # category = models.ForeignKey(Category, on_delete=models.CASCADE)
     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room')
     title = models.CharField(max_length=20)
-    current_member = models.IntegerField()
+    current_member = models.TextField()
     want_member = models.TextField()
     finished = models.BooleanField()
     # template = models.ForeignKey(Template, related_name = 'room')
