f1b669c655e4805b67691978091d73f8554e01a9
hailey-jung <hyojhj0319@gmail.com>
2023-05-21 00:20:40 +0900

    쪽지함

--- ddj/ddjapp/models.py
+++ ddj/ddjapp/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
-
-
 class Room(models.Model):
     # category = models.ForeignKey(Category, on_delete=models.CASCADE)
     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room')
@@ -24,3 +21,26 @@ class Template(models.Model):
 
     def __str__(self):
         return self.room.title
+
+class Article(models.Model):
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    category=models.CharField(max_length=50, null=True)
+    time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    article=models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    content=models.TextField()
+
+    def __str__(self):
+        return self.content
+    
+class Recomment(models.Model):
+    comment=models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='recomments')
+    content=models.TextField()
+
+    def __str__(self):
+        return self.content
