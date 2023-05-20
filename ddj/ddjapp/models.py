70bfe20a76bf663e45122e0959d0cd8ccdc0905a
jiho Kim <jimmy7335@gmail.com>
2023-05-20 23:23:21 +0900

    merge models

--- ddj/ddjapp/models.py
+++ ddj/ddjapp/models.py
@@ -5,22 +5,14 @@ from django.db import models
 
 
 class Room(models.Model):
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room')
-    title = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room')
     current_member = models.TextField()
     want_member = models.TextField()
     finished = models.BooleanField()
-    # template = models.ForeignKey(Template, related_name = 'room')
-
-    def __str__(self):
-        return self.title
-    
-class Template(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='template')
     talk_topic = models.CharField(max_length=20)
     age = models.TextField()
     give_food = models.TextField()
+    sex = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.room.title
+        return self.give_food
