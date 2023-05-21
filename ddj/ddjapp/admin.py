63e3f0b6a05bb2dc1e7f66b11a9c00742acb889a
FULLMOON <boreum@iboleum-ui-MacBookAir.local>
2023-05-21 13:00:00 +0900

    임시저장

--- ddj/ddjapp/admin.py
+++ ddj/ddjapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Room, Article, Comment
+from .models import Room, Article, Comment, Request
 
 
 # Register your models here.
 admin.site.register(Room)
+admin.site.register(Request)
 admin.site.register(Article)
 admin.site.register(Comment)
